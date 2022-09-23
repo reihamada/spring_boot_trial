29623c4ab5ec4c4f5075c22e8001c79f28bd0e84
Rei Hamada <hamada.rei@nci.co.jp>
2022-09-23 15:21:12 +0900

    foo

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,13 +18,13 @@ public class HelloController {
 	private JdbcTemplate jdbcTemplate;
 	org.slf4j.Logger logger = LoggerFactory.getLogger(HelloController.class);
 
-	@GetMapping("/index")
-	public String index() {
+	@GetMapping("/")
+	public String index(Model model) {
 		logger.error("XXXXX");
 		String sql = "SELECT * FROM sakila.address";
 		logger.error(sql);
 		List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
-		// model.addAttribute("testList", list);
+		model.addAttribute("testList", list);
 		return "index";
 	}
 }

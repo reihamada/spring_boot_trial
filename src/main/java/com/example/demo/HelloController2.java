d4d97d9ecad7716b08eb37e2049c9071c524b946
Rei Hamada <hamada.rei@nci.co.jp>
2022-09-23 15:07:03 +0900

    foo

--- src/main/java/com/example/demo/HelloController2.java
+++ src/main/java/com/example/demo/HelloController2.java
@@ -7,23 +7,24 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping()
-public class HelloController {
+@RequestMapping("/")
+public class HelloController2 {
 	@Autowired
 	private JdbcTemplate jdbcTemplate;
-	org.slf4j.Logger logger = LoggerFactory.getLogger(HelloController.class);
+	org.slf4j.Logger logger = LoggerFactory.getLogger(HelloController2.class);
 
-	@GetMapping("/index")
-	public String index() {
+	@GetMapping("/index2")
+	public String index(Model model) {
 		logger.error("XXXXX");
 		String sql = "SELECT * FROM sakila.address";
 		logger.error(sql);
 		List<Map<String, Object>> list = jdbcTemplate.queryForList(sql);
-		// model.addAttribute("testList", list);
+		model.addAttribute("testList", list);
 		return "index";
 	}
 }

c7b0f067d7b908b22306ee7891cf47e2eccaa600
Rei Hamada <hamada.rei@nci.co.jp>
2022-09-24 07:57:35 +0900

    API化(序)

--- src/main/java/com/example/demo/controller/AddressController.java
+++ src/main/java/com/example/demo/controller/AddressController.java
@@ -3,24 +3,24 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.model.Address;
 import com.example.demo.repository.AddressRepository;
 
-@Controller
+@RestController
 @RequestMapping()
 public class AddressController {
 	@Autowired
 	AddressRepository addressRepository;
 
 	@RequestMapping(value = "/Address", method = RequestMethod.GET)
-	public String index(Model model) {
+	public List<Address> index(Model model) {
 		List<Address> addressList = addressRepository.findAll();
 		model.addAttribute("testList", addressList);
-		return "index";
+		return addressList;
 	}
 }
--- src/main/java/com/example/demo/model/Address.java
+++ src/main/java/com/example/demo/model/Address.java
@@ -1,7 +1,5 @@
 package com.example.demo.model;
 
-import java.sql.Timestamp;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,37 +7,37 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import org.springframework.data.geo.Point;
-
 @Entity
 @Table(name = "address")
 public class Address {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 
+	// TODO: public why???
 	@Column(name = "address_id")
 	public Long address_id;
 
 	@Column(name = "address")
 	public String address;
-	@Column(name = "address2")
-	private String address2;
 
-	@Column(name = "district")
-	private String district;
-
-	@Column(name = "city_id")
-	private Integer city_id;
-
-	@Column(name = "postal_code")
-	private String postal_code;
-
-	@Column(name = "phone")
-	private String phone;
-
-	@Column(name = "locationpoint", columnDefinition = "POINT") 
-	private Point location;
-
-	@Column(name = "last_update")
-	private Timestamp last_update;
+//	@Column(name = "address2")
+//	private String address2;
+//
+//	@Column(name = "district")
+//	private String district;
+//
+//	@Column(name = "city_id")
+//	private Integer city_id;
+//
+//	@Column(name = "postal_code")
+//	private String postal_code;
+//
+//	@Column(name = "phone")
+//	private String phone;
+//
+//	@Column(name = "locationpoint", columnDefinition = "POINT") 
+//	private Point location;
+//
+//	@Column(name = "last_update")
+//	private Timestamp last_update;
 }
